200938606e4d1b40024288cfb5daa985c489dbc5
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-05-11 18:37:07 +0300

    main

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,5 @@
 import css from "./ContactList.module.css";
-// import { HiMiniUserCircle, HiMiniPhone } from "react-icons/hi2";
+import { HiMiniUserCircle, HiMiniPhone } from "react-icons/hi2";
 
 export default function ContactList({ allUsers, searchBoxValue, onDeleteUser}) {
 
@@ -7,21 +7,15 @@ export default function ContactList({ allUsers, searchBoxValue, onDeleteUser}) {
         const userId = event.currentTarget.parentNode.getAttribute("data-id");
         onDeleteUser(userId)
     }
+
+  
     return ( 
         <div className={css.container}>
             <ul className={css.list}>
-                {searchBoxValue === "" 
-                    ? allUsers.map((user) => (
-                          <li key={user.id} className={css.item} data-id={user.id}>
-                            <h3 className={css.title}>{user.name}</h3>
-                            <h3 className={css.title}>{user.number}</h3>
-                            <button onClick={handleClick}>Delete</button>
-                          </li>
-                      ))
-                    : allUsers.filter((user) => user.name.includes(searchBoxValue)).map((filteredUser) => ( 
+                     {allUsers.filter((user) => user.name.includes(searchBoxValue)).map((filteredUser) => ( 
                           <li key={filteredUser.id} className={css.item} data-id={filteredUser.id}> 
-                            <h3 className={css.title}>{filteredUser.name}</h3>
-                            <h3 className={css.title}>{filteredUser.number}</h3>
+                            <h3 className={css.title}><HiMiniUserCircle /> {filteredUser.name}</h3>
+                            <h3 className={css.title}><HiMiniPhone/> {filteredUser.number}</h3>
                             <button onClick={handleClick}>Delete</button>
                           </li>
                       ))}
@@ -29,7 +23,3 @@ export default function ContactList({ allUsers, searchBoxValue, onDeleteUser}) {
         </div>
     );
 }
-
-
-{/* <HiMiniUserCircle /> 
-<HiMiniPhone/>   */}
